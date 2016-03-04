ac7ffbde6a1fae486c9ff5374334e3817b598f9d
Nader Aeinehchi <nader@naderica.org>
2016-03-04 15:38:08 +0100

    removed licence..

--- src/java/concurrency/mistakes/doublechecklocking1/Solution1.java
+++ src/java/concurrency/mistakes/doublechecklocking1/Solution1.java
@@ -1,8 +1,3 @@
-/*
- * This software is produced by Nader Aeinehchi. Unauthorized redistribution, reproduction or usage of this
- * software in whole or in part without the express written consent of Nader Aeinehchi is strictly
- * prohibited. Copyright 2016 Nader Aeinehchi. ALL RIGHTS RESERVED
- */
 package java.concurrency.mistakes.doublechecklocking1;
 
 public class Solution1 {
